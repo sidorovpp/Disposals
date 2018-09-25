cdc1149659a263262c6cfbd043f06ac419d0d348
sidorovpp <sidorovpp@mail.ru>
2018-09-25 16:04:24 +0300

    mrcpp

--- libs/applibs/toast/androidtoast/androidtoast.py
+++ libs/applibs/toast/androidtoast/androidtoast.py
@@ -15,20 +15,9 @@ Copyright © 2010-2018 HeaTTheatR
 
 from jnius import autoclass, PythonJavaClass, java_method, cast
 from android.runnable import run_on_ui_thread
-import threading
-import jnius
 
 Toast = autoclass('android.widget.Toast')
 context = autoclass('org.kivy.android.PythonActivity').mActivity
-
-#заглушка для ошибки при выходе
-orig_thread_run = threading.Thread.run
-def thread_check_run(*args, **kwargs):
-    try:
-        return orig_thread_run(*args, **kwargs)
-    finally:
-        jnius.detach()
-threading.Thread.run = thread_check_run
 
 @run_on_ui_thread
 def toast(text, length_long=False):
