f41cce115b458adf0be2cccccf58fd07d591c497
sidorovpp <sidorovpp@mail.ru>
2018-09-25 13:36:28 +0300

    mrcpp

--- libs/applibs/toast/androidtoast/androidtoast.py
+++ libs/applibs/toast/androidtoast/androidtoast.py
@@ -21,9 +21,8 @@ context = autoclass('org.kivy.android.PythonActivity').mActivity
 
 @run_on_ui_thread
 def toast(text, length_long=False):
-    pass
-    #duration = Toast.LENGTH_LONG if length_long else Toast.LENGTH_SHORT
-    #String = autoclass('java.lang.String')
-    #c = cast('java.lang.CharSequence', String(text))
-    #t = Toast.makeText(context, c, duration)
-    #t.show()
+    duration = Toast.LENGTH_LONG if length_long else Toast.LENGTH_SHORT
+    String = autoclass('java.lang.String')
+    c = cast('java.lang.CharSequence', String(text))
+    t = Toast.makeText(context, c, duration)
+    t.show()
