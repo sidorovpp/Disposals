40e6243c9491e4e855eb168502f129012f46e404
sidorovpp <sidorovpp@mail.ru>
2018-09-26 19:08:39 +0300

    mrcpp

--- disposals.py
+++ disposals.py
@@ -105,15 +105,41 @@ class Disposals(App):
             pass
 
     def test(self, *args):
-        from jnius import autoclass, cast
-        PythonActivity = autoclass('org.kivy.android.PythonActivity')
-        Intent = autoclass('android.content.Intent')
-        intent = Intent(Intent.ACTION_MAIN)
-        intent.addCategory(Intent.CATEGORY_HOME)
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-        currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
-        currentActivity.startActivity(intent)
+        #запускаем сервис
+        if platform == 'android':
+            import jnius
+            Context = jnius.autoclass('android.content.Context')
+            Intent = jnius.autoclass('android.content.Intent')
+            PendingIntent = jnius.autoclass('android.app.PendingIntent')
+            AndroidString = jnius.autoclass('java.lang.String')
+            NotificationBuilder = jnius.autoclass('android.app.Notification$Builder')
+            Notification = jnius.autoclass('android.app.Notification')
+            service_name = 'S1'
+            package_name = 'com.something'
+            service = jnius.autoclass('org.kivy.android.PythonService').mService
+            PythonActivity = jnius.autoclass('org.kivy.android' + '.PythonActivity')
+            notification_service = service.getSystemService(
+                Context.NOTIFICATION_SERVICE)
+            app_context = service.getApplication().getApplicationContext()
+            notification_builder = NotificationBuilder(app_context)
+            title = AndroidString("EzTunes".encode('utf-8'))
+            message = AndroidString("Ready to play music.".encode('utf-8'))
+            app_class = service.getApplication().getClass()
+            notification_intent = Intent(app_context, PythonActivity)
+            notification_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                         Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
+            notification_intent.setAction(Intent.ACTION_MAIN)
+            notification_intent.addCategory(Intent.CATEGORY_LAUNCHER)
+            intent = PendingIntent.getActivity(service, 0, notification_intent, 0)
+            notification_builder.setContentTitle(title)
+            notification_builder.setContentText(message)
+            notification_builder.setContentIntent(intent)
+            Drawable = jnius.autoclass("{}.R$drawable".format(service.getPackageName()))
+            icon = getattr(Drawable, 'icon')
+            notification_builder.setSmallIcon(icon)
+            notification_builder.setAutoCancel(True)
+            new_notification = notification_builder.getNotification()
+            service.startForeground(1, new_notification)
 
     def check_disposals(self):
 
@@ -156,14 +182,6 @@ class Disposals(App):
         #меню
         self.nav_drawer = self.screen.ids.nav_drawer
         self.screen.ids.base.add_refresh_button()
-
-
-        #запускаем сервис
-        if platform == 'android':
-            from android import AndroidService
-            service = AndroidService('disposals service', 'running')
-            service.start('service started')
-            self.service = service
 
         Clock.schedule_interval(lambda dt: self.check_disposals(), 300)
 
