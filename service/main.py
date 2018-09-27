64aa1a7ff6fbe3dbff93aaacce2a7011cd4f0fe1
sidorovpp <sidorovpp@mail.ru>
2018-09-27 13:01:35 +0300

    mrcpp

--- service/main.py
+++ service/main.py
@@ -10,18 +10,19 @@ import traceback
 from shutil import copyfile
 
 def check_disposals():
-     with open('/sdcard/disposals/error.txt', 'w+') as f:
-         f.write('on check')
-#     from plyer import notification
-#     from plyer.utils import platform
-#
-#     res = GetResult('getDisposalList', {'readed': 0}, ['Number'])
-#     if len(res) > 0:
-#         title = 'Есть непрочитанные задачи'
-#         message = 'Непрочитанных задач:' + str(len(res))
-#         ticker = 'Уведомление'
-#         kwargs = {'title': title, 'message': message}
-#         kwargs['app_name'] = 'disposals'
+    res = GetResult('getDisposalList', {'readed': 0}, ['Number'])
+    if len(res) > 0:
+        with open('/sdcard/disposals/error.txt', 'w+') as f:
+            f.write(str(len(res)))
+
+#from plyer import notification
+#from plyer.utils import platform
+
+        # title = 'Есть непрочитанные задачи'
+        # message = 'Непрочитанных задач:' + str(len(res))
+        # ticker = 'Уведомление'
+        # kwargs = {'title': title, 'message': message}
+        # kwargs['app_name'] = 'disposals'
 #         if platform == "win":
 #             kwargs['app_icon'] = join(dirname(realpath(__file__)), 'notify.ico')
 #             kwargs['timeout'] = 4
