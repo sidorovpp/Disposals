293e5cd14777c74a88fc24d9407dbece9a996e7a
sidorovpp <sidorovpp@mail.ru>
2018-09-27 13:30:32 +0300

    mrcpp

--- service/main.py
+++ service/main.py
@@ -9,27 +9,25 @@ import libs.uix.baseclass.disposalsdroid as DisposalsDroid
 import traceback
 from shutil import copyfile
 
+#проверка непрочитанных задач и уведомление
 def check_disposals():
     res = GetResult('getDisposalList', {'readed': 0}, ['Number'])
     if len(res) > 0:
-        with open('/sdcard/disposals/error.txt', 'w+') as f:
-            f.write(str(len(res)))
+        from plyer import notification
+        from plyer.utils import platform
 
-#from plyer import notification
-#from plyer.utils import platform
-
-        # title = 'Есть непрочитанные задачи'
-        # message = 'Непрочитанных задач:' + str(len(res))
-        # ticker = 'Уведомление'
-        # kwargs = {'title': title, 'message': message}
-        # kwargs['app_name'] = 'disposals'
-#         if platform == "win":
-#             kwargs['app_icon'] = join(dirname(realpath(__file__)), 'notify.ico')
-#             kwargs['timeout'] = 4
-#         else:
-#             kwargs['app_icon'] = join(dirname(realpath(__file__)), 'notify.png')
-#             kwargs['ticker'] = ticker
-#         notification.notify(**kwargs)
+        title = 'Есть непрочитанные задачи'
+        message = 'Непрочитанных задач:' + str(len(res))
+        ticker = 'Уведомление'
+        kwargs = {'title': title, 'message': message}
+        kwargs['app_name'] = 'disposals'
+        if platform == "win":
+            kwargs['app_icon'] = join(dirname(realpath(__file__)), 'notify.ico')
+            kwargs['timeout'] = 4
+        else:
+            kwargs['app_icon'] = join(dirname(realpath(__file__)), 'notify.png')
+            kwargs['ticker'] = ticker
+        notification.notify(**kwargs)
 
 
 if __name__ == '__main__':
@@ -37,14 +35,8 @@ if __name__ == '__main__':
         config = ConfigParser()
         config.read(join(dirname(realpath(__file__)), pardir,  'disposals.ini'))
         DisposalsDroid.server = config.get('General', 'ip')
-        with open('/sdcard/disposals/server.txt', 'w+') as f:
-            f.write(DisposalsDroid.server)
         DisposalsDroid.username = config.get('General', 'user')
-        with open('/sdcard/disposals/user.txt', 'w+') as f:
-            f.write(DisposalsDroid.username)
         DisposalsDroid.password = config.get('General', 'password')
-        with open('/sdcard/disposals/password.txt', 'w+') as f:
-            f.write(DisposalsDroid.password)
 
         while True:
             sleep(5)
@@ -52,6 +44,5 @@ if __name__ == '__main__':
 
 
     except Exception as E:
-        with open('/sdcard/disposals/error.txt', 'w+') as f:
+        with open('/sdcard/disposals/service_error.log', 'w+') as f:
             f.write(str(E))
-        traceback.print_exc(file=open('sdcard/disposals/error.log','w'))
