247000efb7fe47f2bd93c4b1ce4f7da26009b5ff
sidorovpp <sidorovpp@mail.ru>
2018-09-27 10:36:24 +0300

    mrcpp

--- service/main.py
+++ service/main.py
@@ -6,6 +6,8 @@ from os.path import pardir
 from os.path import realpath
 from kivy.config import ConfigParser
 import libs.uix.baseclass.disposalsdroid as DisposalsDroid
+import traceback
+from shutil import copyfile
 
 def check_disposals():
     from plyer import notification
@@ -30,10 +32,16 @@ def check_disposals():
 if __name__ == '__main__':
     config = ConfigParser()
     while True:
-        config.read(join(dirname(realpath(__file__)), 'disposals.ini'))
+        config.read(join(dirname(realpath(__file__)), pardir,  'disposals.ini'))
         DisposalsDroid.server = config.get('General', 'ip')
         DisposalsDroid.username = config.get('General', 'user')
         DisposalsDroid.password = config.get('General', 'password')
 
         sleep(60)
-        check_disposals()
+        try:
+            check_disposals()
+        except:
+            traceback.print_exc(file=open(join(dirname(realpath(__file__)), pardir,   'error.log'), 'w'))
+            copyfile(join(app.directory, 'error.log'),
+                     join('/sdcard', 'disposals', 'error.log')
+                     )
