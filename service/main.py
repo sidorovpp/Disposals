65b265791bdec945035d1e6f5e48ab4a8ddbc84c
sidorovpp <sidorovpp@mail.ru>
2019-09-16 08:25:51 +0300

    mrcpp

--- service/main.py
+++ service/main.py
@@ -34,12 +34,18 @@ def check_disposals(count):
     else:
         show_notification(title, message)
         if (len(res) > 0) and (len(res) != count):
-            # вибрируем
-            vibrator.vibrate(1)
-            sleep(1)
-            vibrator.cancel()
-            #звук
-            play_sound()
+            from jnius import autoclass
+            AudioManager = autoclass('android.media.AudioManager')
+            Context = jnius.autoclass('android.content.Context')
+            audioManager = Context.AUDIO_SERVICE
+            if audioManager.getRingerMode() == AudioManager.RINGER_MODE_NORMAL:
+                #звук
+                play_sound()
+            elif audioManager.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE:
+                # вибрируем
+                vibrator.vibrate(1)
+                sleep(1)
+                vibrator.cancel()
 
     return len(res)
 
