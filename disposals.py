da7b01470b7b0e7fe98c865b0f50b5707af96e7b
sidorovpp <sidorovpp@mail.ru>
2020-03-26 13:41:13 +0300

    mrcpp home

--- disposals.py
+++ disposals.py
@@ -156,11 +156,65 @@ class Disposals(App):
     def dismiss_popup(self):
         self._popup.dismiss()
 
+    def acquire_permissions(self, permissions, timeout=30):
+        """
+        blocking function for acquiring storage permission
+
+        :param permissions: list of permission strings , e.g. ["android.permission.READ_EXTERNAL_STORAGE",]
+        :param timeout: timeout in seconds
+        :return: True if all permissions are granted
+        """
+
+        import time
+        import functools
+        import jnius
+
+        PythonActivity = jnius.autoclass('org.kivy.android.PythonActivity')
+        Compat = jnius.autoclass('android.support.v4.content.ContextCompat')
+        currentActivity = jnius.cast('android.app.Activity', PythonActivity.mActivity)
+
+        checkperm = functools.partial(Compat.checkSelfPermission, currentActivity)
+
+        def allgranted(permissions):
+            """
+            helper function checks permissions
+            :param permissions: list of permission strings
+            :return: True if all permissions are granted otherwise False
+            """
+            return functools.reduce(lambda a, b: a and b,
+                          [True if p == 0 else False for p in map(checkperm, permissions)]
+                          )
+
+        haveperms = allgranted(permissions)
+        if haveperms:
+            # we have the permission and are ready
+            return True
+
+        # invoke the permissions dialog
+        currentActivity.requestPermissions(permissions, 0)
+
+        # now poll for the permission (UGLY but we cant use android Activity's onRequestPermissionsResult)
+        t0 = time.time()
+        while time.time() - t0 < timeout and not haveperms:
+            # in the poll loop we could add a short sleep for performance issues?
+            haveperms = allgranted(permissions)
+
+        return haveperms
+
     def test(self, *args):
-        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
+        if platform == 'android':
+            perms = ["android.permission.READ_EXTERNAL_STORAGE",
+                     "android.permission.WRITE_EXTERNAL_STORAGE"]
+
+            haveperms = self.acquire_permissions(perms)
+        else:
+            haveperms = True
+
+        if haveperms:
+            content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+            self._popup = Popup(title="Load file", content=content,
+                                size_hint=(0.9, 0.9))
+            self._popup.open()
 
         #if platform == 'android':
         #    from jnius import autoclass
